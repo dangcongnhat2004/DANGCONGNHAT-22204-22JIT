4e658d093a00c0a4d39f83972df3751ebd5925fd
DANG CONG NHAT <dangcongnhat2004qb@gmail.com>
2024-05-15 07:49:12 +0700

    update setting interface

--- app/src/main/java/com/example/a22it204_dangcongnhat_22jit/ManageFragment.kt
+++ app/src/main/java/com/example/a22it204_dangcongnhat_22jit/ManageFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
